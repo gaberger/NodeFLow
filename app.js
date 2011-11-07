45d27f88958f2a56c8a0008c02881f67c2bdeb0d
Gary Berger <gaberger@cisco.com>
2011-11-07 10:10:07 -0500

    Factored out controller

--- app.js
+++ app.js
@@ -6,98 +6,63 @@ var express = require('express')
  var stylus = require('stylus')
  var nib = require('nib')
  var io = require('socket.io')
- var ofc = require('./lib/nodeflow.js')
-var ofl = require('./lib/oflib.js') 
-var ofm = require('./lib/ofmessage.js')
-var decoder = require('./lib/decoder.js')
-var util = require('util');
+ var nc = require('./nodeController.js')
+ var util = require('util');
 
-//Event Handlers
+//Globals
 var address = '10.8.3.119'
 var port = 6633
- 
-//Initialize Controller
-
-controller = ofc.createServer(address, port) 
 
 
-//Event Listeners
+//Initialize Express
+var app = express.createServer()
+//Configure Express
+ app.configure(function() {
+    app.use(stylus.middleware({
+        src: __dirname + '/public',
+        compile: compile
+    }))
+    app.use(express.static(__dirname + '/public'));
+    app.set('views', __dirname);
+    app.set('view engine', 'jade');
 
-controller.on('HELLO', function(socket, data) { 
-    controller.sendMessage(socket, ofm.hello) 
-	controller.sendMessage(socket, ofm.featurerequest)
+    function compile(str, path) {
+        return stylus(str)
+        .set('filename', path)
+        .use(nib());
+    };
+});
 
-})  
-controller.on('ECHO', function(socket, data) { 
-    controller.sendMessage(socket, ofm.echo)
+//Routes
+app.get('/',
+function(req, res) {
+    res.render('index', {
+        layout: false
+    });
+});
 
-})  
-controller.on('FEATUREREPLY', function(socket, data) {
-	var buf = ofm.unpackFeaturesReply(data)
-	console.log(buf)     
-})
-        
-controller.on('PACKETIN', function(socket, data) { 
-    var packetIn = ofm.unpackPacketIn(data)
-   	var pack_buf = ofm.unpackPacketBuffer(data, packetIn.length) 
-   	var pbuf = decoder.decodeethernet(pack_buf, 0) 
-    console.log(packetIn)
-    console.log(pbuf) 
-                                             
-    	
-})  
+app.get('/riak',
+function(req, res) {
+    res.render('db', {
+        layout: false
+    });
+});
 
-controller.on('FLOWMOD', function(socket, data){
-	console.log(data)
-})
+//Listeners
+app.listen(3000,
+function() {
+    var addr = app.address();
+    util.log('WebServer listening on http://' + addr.address + ':' + addr.port);
+});                                
 
+var controller = nc.startController(address, port)
 
-// //Initialize Express
-// var app = express.createServer()
-// //Configure Express
-//  app.configure(function() {
-//     app.use(stylus.middleware({
-//         src: __dirname + '/public',
-//         compile: compile
-//     }))
-//     app.use(express.static(__dirname + '/public'));
-//     app.set('views', __dirname);
-//     app.set('view engine', 'jade');
+  
+io.listen(app)
 // 
-//     function compile(str, path) {
-//         return stylus(str)
-//         .set('filename', path)
-//         .use(nib());
-//     };
-// });
 // 
-// //Routes
-// app.get('/',
-// function(req, res) {
-//     res.render('index', {
-//         layout: false
-//     });
-// });
-// 
-// app.get('/riak',
-// function(req, res) {
-//     res.render('db', {
-//         layout: false
-//     });
-// });
-// 
-// //Listeners
-// app.listen(3000,
-// function() {
-//     var addr = app.address();
-//     console.log('   WebServer listening on http://' + addr.address + ':' + addr.port);
-// });
-// 
-//        
-//
-//
 //  io.sockets.on('connection',
 // function(socket) {
 //     var controllerInstance = server.startServer(socket)
-// });
-//
+// });              
+                                   
---
+++ nodeController.js
@@ -0,0 +1,56 @@
+var controller = require('./lib/nodeflow.js') 
+var ofl = require('./lib/oflib.js')
+var ofm = require('./lib/ofmessage.js')
+var decoder = require('./lib/decoder.js') 
+var ofc = require('./lib/nodeflow.js')
+
+function nodeController() {
+
+    }
+
+module.exports.startController = function(address, port) {  
+
+  console.log(address + ' ' + port)
+  controller = ofc.createServer(address, port)
+
+
+    //Event Listeners
+    ofmessage = {}
+
+    controller.on('OFPT_HELLO',
+    function(socket, data) {
+        ofmessage.OFPT_HELLO = true
+    })
+
+    controller.on('OFPT_ECHO_REQUEST',
+    function(socket, data) {
+        controller.sendMessage(socket, ofm.echo)
+
+    })
+
+    controller.on('OFPT_FEATURES_REPLY',
+    function(socket, data) {
+        ofmessage.OFPT_FEATURES_REPLY = true
+        console.dir(data)
+        // var buf = ofm.unpackFeaturesReply(data)
+    })
+
+    controller.on('OFPT_PORT_STATUS ',
+    function(socket, data) {
+        ofmessage.got_OFPT_PORT_STATUS = true
+        console.dir(data)
+        // var buf = ofm.unpackFeaturesReply(data)
+    })
+
+
+    controller.on('OFPT_PACKET_IN',
+    function(socket, data) {
+        // var packetIn = ofm.unpackPacketIn(data)
+        var pbuf = decoder.decodeethernet(data.buffer, 0)
+        console.dir(pbuf)
+
+    })
+
+    
+}
+
