86d1b8694c17ea2a5b731ff2650423edcc9477e2
Gary Berger <gaberger@cisco.com>
2011-12-06 12:57:04 -0500

    Refactored server with basic pyswitch functionality

--- app.js
+++ app.js
@@ -6,23 +6,23 @@ var util = require('util');
 var oflib = require('./oflib-node')
 
 //Controller Globals
- var address = '10.8.3.119'
-var port = 6633
+var address = '10.8.3.119'
+var port = 6632
 var version = 1
 
 var server = require('./lib/nodeflow-server.js')
 
- var nc = new server.NodeFlow()
- nc.Start(address, port)
-
- 			nc.on('OFPT_HELLO', function(socket, msg) { 
+var nc = new server.NodeFlow()
+ 
+	nc.Start(address, port)  
+	
+    		nc.on('OFPT_HELLO', function(socket, msg) { 
 				   this.version = msg.message.version
 			                    
 		      		  var buf = new Buffer(oflib.ofp.sizes.ofp_header); 
 					    pack = oflib.pack(msg.message, buf, 0) 
 				    	socket.write(buf) 
-						console.dir(buf)  
-				
+			    
 				
 		               	message = {                 
 		               	  		  "header" : {"type" : 'OFPT_FEATURES_REQUEST', "xid" : 1},
@@ -30,11 +30,10 @@ var server = require('./lib/nodeflow-server.js')
 		   				 	      "body" : {}
 		   				  	      };   
                	                        
-					    	var buf = new Buffer(oflib.ofp.sizes.ofp_header); 
-						    pack = oflib.pack(message, buf, 0) 
-					    	socket.write(buf) 
-							console.dir(buf)
-                  
+					   var buf = new Buffer(oflib.ofp.sizes.ofp_header); 
+					   pack = oflib.pack(message, buf, 0) 
+				       socket.write(buf) 
+		          
 			})                    
 
 
@@ -43,19 +42,41 @@ var server = require('./lib/nodeflow-server.js')
         	    var buf = new Buffer(oflib.ofp.sizes.ofp_header);
 			    var pack = oflib.pack(msg.message, buf, 0)   
 		        socket.write(buf) 
-				console.dir(buf)
-			})
+		    })
 
 
 			nc.on('OFPT_PACKET_IN',    function(socket, msg) { 
-				  console.dir(msg)
+		        var message = {
+					    message : {
+					        header: {
+					            type: 'OFPT_PACKET_OUT',
+					            xid: msg.message.header.xid
+					        },
+					        body: {
+					            buffer_id: msg.message.body.buffer_id,
+					            in_port: msg.message.body.in_port,
+					            actions: [
+					            {
+					                header: {  type: 'OFPAT_OUTPUT' },
+					                body: {    port: 'OFPP_FLOOD'   },
+					            }
+					            ]
+					        },
+							version: msg.message.version
+					    }
+					}
 				
-				
-			}) 
+	    		var buf = new Buffer(64);  
+	    	 	var pack = oflib.pack(message.message, buf, 0)   
+			    	if (!('error' in pack)) {     
+			 			//trim buffer
+						var buf2 = buf.slice(0, pack.offset);
+			            socket.write(buf2)
+				    }    
+	        }) 
 			
 			nc.on('OFPT_FEATURES_REPLY', function(socket, msg) {
-				 console.dir(msg)
-			})
+		    })
 
 
 
